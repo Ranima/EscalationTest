150d5bf50cc2d6e3914ca7e90327f850d7abb73b
Raphael <Raphael@PLANETSIDE>
2017-01-08 21:42:53 -0800

    still needs more work

--- opening_a_file/opening a file.cpp
+++ opening_a_file/opening a file.cpp
@@ -15,8 +15,16 @@ void main()
 {
 	std::fstream file;
 	Things monster;
-
-	
+	std::fstream fin("EscalationProgrammerTest.bin", std::ios::in | std::ios::binary);
+	if (fin.good()) {
+		while (!fin.eof() && fin.peek() != EOF) {
+			fin.read((char*)&monster, sizeof(Things));
+			std::cout << monster.uID << "\n" << monster.typeID << "\n" <<
+				monster.x << " " << monster.y << "\n" << monster.nx << " " << monster.ny;
+		}
+		fin.close();
+	}
+	system("pause");
 }
 
 //IntroToCPP/Topics/TextFiles/TextFileIO
